f5bbd0a33d09ce8b98e1d986e538960b15efa4ff
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-05-18 22:10:56 +0200

    remove CLI utils

--- routes.go
+++ routes.go
@@ -11,18 +11,10 @@ func root(w http.ResponseWriter, req *http.Request) {
 	type page struct {
 		Title        string
 		WebUtilities []string
-		CliUtilities map[string]string
 	}
 	p := page{
 		Title:        "Utilities",
 		WebUtilities: []string{"addr", "headers", "status200", "status302", "status500"},
-		CliUtilities: map[string]string{
-			"~/bin":      "https://github.com/jreisinger/dotfiles/tree/master/bin",
-			"checkip":    "https://github.com/jreisinger/checkip",
-			"runp":       "https://github.com/jreisinger/runp",
-			"waf-runner": "https://github.com/jreisinger/waf-runner",
-			"waf-tester": "https://github.com/jreisinger/waf-tester",
-		},
 	}
 	t, err := template.New("page.html").ParseFiles("template/page.html")
 	if err != nil {
